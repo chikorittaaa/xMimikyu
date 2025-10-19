485128131f5787aff3c7cce9dfd7ebb69b5078f6
chikorittaaa <chikoritaisdope1@gmail.com>
2025-10-19 10:37:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# Configuration file for the Discord bot
+
+# Bot prefix (can be changed anytime)
+PREFIX = '!'
+
+# Embed color (you can change this to any hex color)
+EMBED_COLOR = 0xfeb1d3
+
+# Pagination settings
+IDS_PER_PAGE = 200  # Number of IDs to show per page
+
+# Recording timeout (in seconds) - auto-stop if no activity
+RECORDING_TIMEOUT = 120  # 10 minutes (600 seconds)
+
+# How often to check for inactivity (in seconds)
+INACTIVITY_CHECK_INTERVAL = 30  # Check every 30 seconds
